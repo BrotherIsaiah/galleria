31da8abdad2ed616426522f15e230b98894110a2
Isaiah <isaiah.ekue@protonmail.com>
2024-06-23 18:03:36 +0200

    created app

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './Components/Header'
+import './App.css';
+import TheHeader from './Components/Header';
+
+function App() {
+  return (
+    <TheHeader/>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,9 @@
+import './Header.css'
+export default function TheHeader() {
+  return (
+    <header>
+      <h1>galleria.</h1>
+      <p>Art and history</p>
+    </header>
+  );
+}
